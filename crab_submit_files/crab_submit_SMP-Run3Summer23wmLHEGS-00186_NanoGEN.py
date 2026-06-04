22aece89c8c0442633584d004dbc2ea477c5b64f
oponcet <oceane.poncet@cern.ch>
2026-06-04 12:07:56 -0500

    Adapt to LPC and test with few events

--- crab_submit_files/crab_submit_SMP-Run3Summer23wmLHEGS-00186_NanoGEN.py
+++ crab_submit_files/crab_submit_SMP-Run3Summer23wmLHEGS-00186_NanoGEN.py
@@ -1,25 +1,39 @@
 from CRABClient.UserUtilities import config
 config = config()
 
+# -------------------------
+# General
+# -------------------------
 config.General.requestName = 'SMP-Run3Summer23wmLHEGS-00186-NanoGEN'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
+# -------------------------
+# Job type
+# -------------------------
 config.JobType.pluginName = 'PrivateMC'
 config.JobType.psetName = '../configs/SMP-Run3Summer23wmLHEGS-00186_NanoGEN_cfg.py'
 config.JobType.allowUndistributedCMSSW = True
 config.JobType.numCores = 1
-config.JobType.maxMemoryMB = 5000
+config.JobType.maxMemoryMB = 3000
 
+# -------------------------
+# Data (GEN production)
+# -------------------------
 config.Data.outputPrimaryDataset = 'SMP-Run3Summer23wmLHEGS-00186'
-config.Data.splitting = 'EventBased'
-config.Data.unitsPerJob = 100
-NJOBS = 5000
-config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
-# Must be changed to your own EOS area before submission.
-config.Data.outLFNDirBase = '/store/user/YOUR_USERNAME/'
-config.Data.publication = False
-config.Data.outputDatasetTag = 'Run3Summer23_NanoGEN_v1'
 
-config.Site.storageSite = 'T2_CH_CERN'
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 20
+config.Data.totalUnits = 100
+# -------------------------
+# Output
+# -------------------------
+config.Data.outLFNDirBase = '/store/user/oponcet/'
+config.Data.publication = False
+config.Data.outputDatasetTag = 'Run3Summer23_NanoGEN_v2'
+
+# -------------------------
+# Site
+# -------------------------
+config.Site.storageSite = 'T3_US_FNALLPC'
